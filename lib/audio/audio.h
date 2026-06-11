6df15ebd4a54cbf6419b86be32c681ff456e4054
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-06-11 11:20:29 +0200

    Changed the guards to 'audio.h'

--- lib/audio/audio.h
+++ lib/audio/audio.h
@@ -1,7 +1,10 @@
-#pragma once
+#ifndef AUDIO_H
+#define AUDIO_H
 
 #include <Arduino.h>
 
 bool audio_init();
 void audioLoop();
 void audio_write(const uint8_t* payload, size_t length);
+
+#endif //AUDIO_H
