b1937d48758db93c359881fd4709cf2545254f01
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-06-14 23:11:41 +0200

    Created a simple main.cpp file to test the event_queue library.

--- src/envs/test_queue/main.cpp
+++ src/envs/test_queue/main.cpp
@@ -1,19 +1,49 @@
 #include <Arduino.h>
 
 #include "led_counter.h"
-//#include "event_queue.h"
+#include "event_queue.h"
+#include "config.h"
 
 Led led0(pin_led0), led1(pin_led1), led2(pin_led2);
 Led* leds[] = {&led0, &led1, &led2};
 LedCounter led_counter(leds, 3);
 
+EventQueue event_queue;
+
 void setup()
 {
-  led_counter.init(); 
+  Serial.begin(baud_rate);
+  
+  led_counter.init();
+  led_counter.reset();
+  
+  Serial.println("=== Event Queue Test ===");
+  
+  // Push 10 events
+  Serial.println("Pushing 10 events...");
+  for (int i = 0; i < 10; i++) {
+    event_queue.push(Event::boot_request);
+    led_counter.increment();
+    delay(500);
+    Serial.println(led_counter.getValue());
+  }
+  
+  delay(2000);
+  
+  // Pop 10 events
+  Serial.println("Popping 10 events...");
+  for (int i = 0; i < 10; i++) {
+    returned_event_t result = event_queue.pop();
+    if (result.is_valid) {
+      led_counter.decrement();
+      delay(500);
+      Serial.println(led_counter.getValue());
+    }
+  }
+  
+  Serial.println("Test complete!");
 }
 
 void loop()
 {
-   led_counter.increment();
-   delay(1000);
 }
