a7b6e53cf639157ae111941502a358e03d69da30
Erasmus Cedernaes <github@emaus.dev>
2026-08-25 23:04:19 +0200

    Use standard include pattern

--- lib/wifi_manager/wifi_manager.h
+++ lib/wifi_manager/wifi_manager.h
@@ -1,5 +1,5 @@
-#ifndef WIFI_MANAGER
-#define WIFI_MANAGER
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
 
 void wifi_manager_init();
 void wifi_provisioning_service();
@@ -7,4 +7,4 @@ void wifi_redirect_service();
 void wifi_redirect_stop();
 void start_ip_info_portal(void);
 
-#endif //WIFI_MANAGER
+#endif //WIFI_MANAGER_H
